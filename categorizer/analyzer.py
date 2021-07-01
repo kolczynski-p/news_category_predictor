8f7b7dd94db6f74043765816be66bd42f24af38a
jblazlik <jblazlik@edu.cdv.pl>
2021-07-01 23:16:35 +0200

    Add "World count" function to Analyzer

--- categorizer/analyzer.py
+++ categorizer/analyzer.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import json
 import nltk
+import matplotlib.pyplot as plt
+import matplotlib
 from nltk.corpus import stopwords
 import textblob         
 from textblob import TextBlob
@@ -13,7 +15,20 @@ from textblob import Word
 
 class Analyzer():
 
-   def describe(self):
-        print(self.data.info())
+    def __init__(self, dataframe=pd.DataFrame(), lang='english'):
+        self.data = dataframe
+        nltk.download('stopwords')
+        nltk.download('wordnet')
+        self.stop = stopwords.words(lang)
+
+    def word_count(self):
+        self.data['word_count'] = self.data['text'].apply(lambda x: len(str(x).split(" ")))
+        patches = plt.hist(self.data['word_count'], 150, density=1, facecolor='g', alpha=0.75)
+        plt.xlabel('Word count')
+        plt.ylabel('probability densit')
+        plt.title('Histogram of word amount')
+        plt.xlim(0, 50)
+        plt.grid(True)
+        plt.show()
         
 
