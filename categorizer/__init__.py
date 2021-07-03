5969e2cec2f823302fdd765ba45f488520275bed
pat-pom <81462784+pat-pom@users.noreply.github.com>
2021-07-03 17:11:09 +0200

    vectorization added

--- categorizer/__init__.py
+++ categorizer/__init__.py
@@ -1,2 +1,3 @@
 from categorizer.normalizer import Normalizer
 from categorizer.analyzer import Analyzer
+from categorizer.vectorization import Vectorizer
---
+++ categorizer/vectorization.py
@@ -0,0 +1,24 @@
+import categorizer
+import numpy as np
+import pandas as pd
+import json
+import nltk
+import matplotlib.pyplot as plt
+import matplotlib
+from nltk.corpus import stopwords
+import textblob         
+from textblob import TextBlob
+from textblob import Word
+
+        
+
+
+class Vectorizer():
+
+    def __init__(self, dataframe=pd.DataFrame(), lang='english'):
+        self.data = dataframe
+        nltk.download
+
+
+    def tol():
+        print("lbie placko")
